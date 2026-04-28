62f818b459e2392bf3c67eeff4bafbd3a652d805
BALAGA LALIT KISHORE <blalitkishore@gmail.com>
2026-04-28 10:21:02 +0530

    UC9: Implement win checking logic (rows, columns, diagonals)

---
+++ TicTacToeUC9.java
@@ -0,0 +1,33 @@
+public class TicTacToe {
+
+    static char[][] board = {
+        {'X', 'X', 'X'},
+        {'-', 'O', '-'},
+        {'O', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(hasWon('X'));
+    }
+
+    static boolean hasWon(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol)
+                return true;
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol)
+                return true;
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol)
+            return true;
+
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)
+            return true;
+
+        return false;
+    }
+}
